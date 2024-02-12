b63b5f244d746127e1e7b16631a6420fb062b1e2
John <johntheherrera@gmail.com>
2024-02-11 23:36:35 -0800

    buttons work

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 let cat_p = 0;
 let jo_p = 0;
+let score = 0;
 
-function correct() {
+function correct(button) {
+    let test = window.getComputedStyle(button);
+    let color = test.backgroundColor;
+
+    if (color != 'rgb(0, 128, 0)')
+    {
+        button.style.backgroundColor = 'green';
+        score++;
+    }
 
 }
 
-function incorrect() {
-    
+function incorrect(button) {
+    let test = window.getComputedStyle(button);
+    let color = test.backgroundColor;
+
+    if (color != 'rgb(128, 0, 0)')
+    {
+        button.style.backgroundColor = 'red';
+        score--;
+    }
+
 }
